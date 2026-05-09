0a049032e33c93aadcb1fb56bf0f218845835bfc
phpiiper <plp@njit.edu>
2026-05-09 01:06:42 -0400

    Finished frontend design for admin page

--- frontend/components/adminPage.ts
+++ frontend/components/adminPage.ts
@@ -16,6 +16,7 @@ export interface UserType {
   _id: string;
   username: string;
   role: string;
+  email: string;
 }
 
 @Component({
@@ -28,7 +29,6 @@ export class AdminPage implements OnInit{
 
   ngOnInit(){
     this.fetchAdminData().then(res => {
-        console.log(res)
         // IF admin...
         if (Array.isArray(res.logs)) {
           const newLogs = res.logs
@@ -39,7 +39,7 @@ export class AdminPage implements OnInit{
           this.users.set(newUsers)
         }
       })
-    }
+  }
 
   async fetchAdminData(){
     this.fetching.set(true);
@@ -59,7 +59,9 @@ export class AdminPage implements OnInit{
       if (Array.isArray(data.logs)) {
         const newLogs = data.logs
         this.logs.set(newLogs)
-        this.filteredAPIs.set([...new Set(newLogs.logs.map(x => x.api))])
+        this.filteredAPIs.set([...new Set(newLogs.map((x: any) => x.api))])
+        this.filteredLogs.set(newLogs)
+        await this.filterSubmit()
       }
       if (Array.isArray(data.users)) {
         const newUsers = data.users
@@ -73,19 +75,38 @@ export class AdminPage implements OnInit{
   loading = signal(true);
   fetching = signal(false);
   canAccess = signal(false);
-  tab = signal(1);
+  tab = signal(0);
   setTab(tabIndex: number){
     this.tab.set(tabIndex);
   }
   adminData = signal(null);
   logs = signal<LogType[]>([]);
-  filteredAPIs = signal<string[]>([]);
+  filteredAPIs = signal<any[]>([]);
   users = signal<UserType[]>([])
   lastUpdated = signal<string>(new Date().toLocaleTimeString());
 
+  filterForm = new FormGroup({
+    level: new FormControl("None", Validators.required),
+    describe: new FormControl('f', Validators.required),
+    api: new FormControl("None", Validators.required),
+  });
+  filteredLogs = signal<any[]>([]);
+  async filterSubmit(){
+    let {level, api} = this.filterForm.value;
+    const filteredLogs = this.logs().filter(x => {
+        if (x.level !== level && level !== "None") {return false}
+        if (x.api !== api && api !== "None") {return false}
+        return true
+    })
+    this.filteredLogs.set(filteredLogs)
+  }
+
   async updateUser(id: string, key: string, value: string){
       this.fetching.set(false)
-      if (!id || !key || !value || !["username","password"].includes(key)) {return}
+      console.log(106, id, key, value)
+      if (!id || !key || !value || !["username","password","email"].includes(key)) {return {
+        error: true, success: false, message: "Missing, or invalid update keys!"
+      }}
       const res = await fetch(`${environment.backend}/api/admin/updateUser`,{
         credentials: "include",
         headers: { "Content-Type": "application/json" },
@@ -97,12 +118,114 @@ export class AdminPage implements OnInit{
       const data = await res.json();
       if (data.error){
         console.log("ERROR")
+        return {error: true, success: false,  message: data.message}
       }
     this.fetching.set(true)
-
-
+    return {error: false, success: true, message: "User updated successfully."};
   }
 
 
+
+
+
+  disableUserFormHandler(choice: boolean){
+    if (choice){
+      this.userForm.get("username")?.disable();
+      this.userForm.get("email")?.disable();
+      this.userForm.get("password")?.disable();
+      this.userFormDisabled.set(true)
+    } else {
+      this.userForm.get("username")?.enable();
+      this.userForm.get("email")?.enable();
+      this.userForm.get("password")?.enable();
+      this.userFormDisabled.set(false)
+    }
+  }
+  userFormDisabled = signal(true);
+  userForm = new FormGroup({
+    id: new FormControl({
+      value: "None", disabled: true
+    }, Validators.required),
+    username: new FormControl({
+      value: "", disabled: this.userFormDisabled()
+    }, Validators.required),
+    email: new FormControl({
+      value: "", disabled: this.userFormDisabled()
+    }, Validators.required),
+    password: new FormControl({
+      value: "", disabled: this.userFormDisabled()
+    }, Validators.required),
+  });
+  changeUser(event: any){
+    const id = typeof event === "object" && event?.target ? (event.target as HTMLInputElement).value : (typeof event === "string" ? event : "");
+    if (id !== "None"){
+        const user = this.users().find(x => x._id === id);
+        if (!user){return;}
+        this.userForm.setValue({
+          id: id || "N/A",
+          username: user.username,
+          email: user.email,
+          password: ""
+        })
+        this.disableUserFormHandler(false);
+    } else {
+      this.userForm.setValue({
+        id: id || "None",
+        username: "",
+        email: "",
+        password: ""
+      })
+      this.disableUserFormHandler(true);
+    }
+  }
+  async updateUserHandler(type: string){
+    const userForm = this.userForm.getRawValue();
+    let updateAll = type === "all"
+    const allowedValues = ["password", "username", "email"] as const;
+    this.disableUserFormHandler(true);
+    let errorMessages = [];
+    let successMessages = [];
+    for (let allowed of allowedValues){
+        if (type === allowed || updateAll){
+          let value = userForm[allowed] || ""
+          if (value.length < 3) {
+            console.log("LENGTH ERROR")
+            continue;
+          }
+          const {error, message} = await this.updateUser(userForm.id || "", allowed, userForm[allowed] || "")
+          if (error) {
+            errorMessages.push(message)
+          } else {
+            successMessages.push(message)
+            this.users.update((users:any) => {
+              return users.map((x: any)=> {
+                if (x._id !== userForm.id) {
+                  return x
+                }
+                return {...x, [allowed]: userForm[allowed]}
+              })
+            })
+          }
+
+        }
+    }
+    this.handleUserUpdateMessages(errorMessages, successMessages);
+    this.disableUserFormHandler(false);
+  }
+
+  handleUserUpdateMessages(errorMessages:string[], successMessages:string[]){
+      let updateError = false;
+      let updateErrorString = ""
+      let updateSuccessString = ""
+      if (errorMessages.length > 0) {
+          updateError = true;
+          updateErrorString = errorMessages.join(", ");
+      }
+      if (successMessages.length > 0) {
+        updateSuccessString = successMessages.join(", ");
+      }
+
+     // console.log(updateError, updateErrorString, updateSuccessString);
+  }
 
 }
